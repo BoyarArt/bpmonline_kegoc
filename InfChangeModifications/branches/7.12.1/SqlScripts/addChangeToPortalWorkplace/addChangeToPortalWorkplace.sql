a9560a45af45a5931191148b6acceca5da32d29f
bpmonline_kegoc_1 <bpmonline_kegoc_1@82e15433-2cb2-2c4e-a284-969ced566d97>
2018-07-09 12:27:27 +0000

    скрипт добавления раздела Изменения на рабочее место портала

---
+++ InfChangeModifications/branches/7.12.1/SqlScripts/addChangeToPortalWorkplace/addChangeToPortalWorkplace.sql
@@ -0,0 +1,9 @@
+INSERT INTO SysModuleInWorkplace
+(Id, ProcessListeners, Position, SysWorkplaceId, SysModuleId)
+VALUES
+  (
+   'd44d57d9-56b1-4d28-aa07-cd14230f6c81',
+   0,
+   17,
+   '714c8d47-ed34-4d4e-9de6-fe8911bddf55',
+   '2d22da5a-641a-4a00-8739-282ae69b67e5');
